523d3c444f4435bd0e23fb11af01bd3427863772
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-16 16:15:37 -0500

    Update views.py

--- Profiles/views.py
+++ Profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HTTPResponse
+
+def index(request):
+  return HTTPResponse()
+  #Placeholder - Redirect to homepage here
