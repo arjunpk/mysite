7869baa3d9fa8399b728b343819a28fd4ca9beb9
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-16 16:15:56 -0500

    Update views.py

--- media/views.py
+++ media/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HTTPResponse
+
+def index(request):
+  return HTTPResponse()
+  #Placeholder - Redirect to homepage here
