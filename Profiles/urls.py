d6552bafe03976c80c651fa28b95508ac8ad0dea
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-19 15:13:53 -0500

    Added entry for change password

--- Profiles/urls.py
+++ Profiles/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
   url(r'^$', views.index, name='index'),
   #Placeholder - need to fix with usernames
   url(r'^album/', include('media.urls')),
+  url(r'^password/$', views.change_password, name='change_password'),
 ]
