0f380b5067737aed341fe93fd524b806916a339b
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-19 16:21:05 -0500

    Update forms.py

--- Profiles/forms.py
+++ Profiles/forms.py
@@ -1,3 +1,13 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from Profiles.models import Profiles
+
+class CreateBasicUserForm(ModelForm):
+  class Meta:
+    model = User
+    fields = { email, first_name, last_name }
+
+class SelectUserTypeForm(ModelForm):
+  class Meta:
+    model = Profiles
+    fields = { user_type }
