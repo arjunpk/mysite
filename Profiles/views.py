48eb080d05ac999b5620024ce0a1cabdc85e9fa3
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-19 15:13:01 -0500

    Added Change Password to view

--- Profiles/views.py
+++ Profiles/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +9,22 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse()
   #Placeholder - Redirect to homepage here
+
+  
+
+
+def change_password(request):
+  if request.method == 'POST':
+    form = PasswordChangeForm(request.user, request.POST)
+    if form.is_valid():
+      user = form.save()
+      update_session_auth_hash(request, user)  # Important!
+      messages.success(request, 'Your password was successfully updated!')
+      return redirect('accounts:change_password')
+    else:
+      messages.error(request, 'Please correct the error below.')
+  else:
+    form = PasswordChangeForm(request.user)
+  return render(request, 'accounts/change_password.html', {
+    'form': form
+  })
