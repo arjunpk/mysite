6de04cc4bad113622ed58a14e7868c28baa1268d
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-19 10:31:37 -0500

    Dec 19 First
    Updated the app media.
    Combined photos and videos into one class
    Added classes for alternative resolutions and media reports

--- media/models.py
+++ media/models.py
@@ -15,30 +15,51 @@ class Albums(models.Model):
   create_date = models.DateTimeField()
   
   
-class Photos(models.Model):
+class Media(models.Model):
   user = models.ForeignKey(User)
   album = models.ForeignKey(Albums)
-  photo_title = models.CharField(max_length=50, blank=True)
-  photo_desc = models.CharField(max_length=150, blank=True)
-  original_image = models.CharField(max_length=100, blank=True)
-  display_image = models.CharField(max_length=100, blank=False)
-  thumb_image = models.CharField(max_length=100, blank=False)
+  MEDIA_TYPE_CHOICES = (
+    (1, 'Photo'),
+    (2, 'Video'),
+  )
+  media_type=models.PositiveSmallIntegerField('Media Type',
+                                              choices=MEDIA_TYPE_CHOICES,
+                                              blank=False,
+                                              null=False)
+  title = models.CharField(max_length=50, blank=True)
+  desc = models.CharField(max_length=150, blank=True)
+  orig_extension = models.CharField(max_length=5, blank=False)
+  #If image is imported from social network, we do not need to store the original image or resize them.
+  #Unless we allow import from 500px if possible and if 500px gives larger file formats
+  original_media_file = models.CharField(max_length=100, blank=True)
+  display_media_file = models.CharField(max_length=100, blank=False)
+  thumb_media_file = models.CharField(max_length=100, blank=False)
   share_to_fb = models.BooleanField()
   share_to_tw = models.BooleanField()
   share_to_ig = models.BooleanField()
   up_votes = models.IntegerField()
   down_votes = models.IntegerField()
 
-class Videos(models.Model):
-  user = models.ForeignKey(User)
-  album = models.ForeignKey(Albums)
-  video_title = models.CharField(max_length=50, blank=True)
-  video_desc = models.CharField(max_length=150, blank=True)
-  original_video = models.CharField(max_length=100, blank=True)
-  display_video = models.CharField(max_length=100, blank=False)
-  thumb_video = models.CharField(max_length=100, blank=False)
-  share_to_fb = models.BooleanField()
-  share_to_tw = models.BooleanField()
-  share_to_ig = models.BooleanField()
+class Media_alt_Res(models.Model):
+  """This class holds media in alternative resolutions. The main class stores the default resolution for the website.
+  In this class, media details of varying resolutions are stored in order to provide the capability of upgrades"""
+  media = models.ForeignKey(Media)
+  RES_CHOICES = (
+    (1, '480p'),
+    (2, '720p'),
+    (3, '1080p'),
+  )
+  resolution = models.PositiveSmallIntegerField('Resolution',
+                                                choices=RES_CHOICES,
+                                                blank=False,
+                                                null=False)
+  media_file = models.CharField(max_length=100, blank=False)
+  
+class Media_Reports(models.Model):
+  """Placeholder. Needs to be modified. This class holds the reports on the media by users for violation of Terms of Use"""
+  media = models.ForeignKey(Media)
+  report = models.CharField(max_length=1000)
+  
+
 
 
