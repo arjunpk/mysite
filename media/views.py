d5b04d32f3f9175e61e67409614d6a5fdc84d848
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-21 10:49:21 -0500

    Update views.py

--- media/views.py
+++ media/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse()
-  #Placeholder - Redirect to homepage here
+  redirect('/')
