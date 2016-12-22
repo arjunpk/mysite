71c8a46235d082c0589130fa6b8fc7bf86611c1f
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-22 14:13:12 -0500

    Update models.py

--- Profiles/models.py
+++ Profiles/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 from django.contrib.auth.models import User
