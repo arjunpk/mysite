212bd1e2c900ef6b2444774ecfba45f3b9b7151b
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-22 16:50:10 -0500

    Update urls.py

--- Profiles/urls.py
+++ Profiles/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns=[
+  #/profile
   url(r'^$', views.index, name='index'),
-  #Placeholder - need to fix with usernames
-  url(r'^album/', include('media.urls')),
-  url(r'^password/$', views.change_password, name='change_password'),
+  #/profile/<uid/username>
+  url(r'^(?P<user_name_id>[A-Za-z0-9_]+)$', views.detail, name='user_profile'),
+  #/profile/<uid/username>/album
+  url(r'^(?P<user_name_id>[A-Za-z0-9_]+)/album/', include('media.urls')),
+  #/profile/reset_password
+  url(r'^reset_password/$', views.change_password, name='change_password'),
 ]
