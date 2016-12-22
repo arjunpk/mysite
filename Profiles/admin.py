f8c36818bd3dfaf54a156a5f5f8ac5fc538208e3
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-22 14:49:45 -0500

    Update admin.py

--- Profiles/admin.py
+++ Profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categories, Countries, States, Cities, ZipCodes, Profile
+
+admin.site.register(Categories)
+admin.site.register(Countries)
+admin.site.register(States)
+admin.site.register(Cities)
+admin.site.register(ZipCodes)
+admin.site.register(Profile)
+
