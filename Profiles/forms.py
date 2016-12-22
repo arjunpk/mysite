e02121c753ea165cd046365f1ab7814d5806ee35
Arjun Parukkandy Kishore <arjunpk@gmail.com>
2016-12-22 14:03:01 -0500

    Update forms.py

--- Profiles/forms.py
+++ Profiles/forms.py
@@ -6,6 +6,12 @@ class CreateBasicUserForm(ModelForm):
   class Meta:
     model = User
     fields = { 'email', 'first_name', 'last_name' }
+    """
+    def verify_email(self):
+		  email = self.cleaned_data.get('email')
+		  if email and User.objects.filter(email=email).count():
+        raise forms.ValidationError(u'There is already an account with this email id.')
+    """
 
 class SelectUserTypeForm(ModelForm):
   class Meta:
